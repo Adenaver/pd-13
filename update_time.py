e18ca4cda2b8ae9767c6d9b89f8c465180cdeabb
zhenyok <zhenyok@mail.ua>
2018-07-13 23:50:09 +0300

    Rollback test

--- update_time.py
+++ update_time.py
@@ -3,14 +3,12 @@ import os
 hostname = os.environ.get('hosting')
 username = os.environ.get('user')
 password = os.environ.get('password')
-database = os.environ.get('bdname')
+database = os.environ.get('dbname')
 def new_time(times,winner):
     con = psycopg2.connect( host=hostname, user=username, password=password, dbname=database )
     cur = con.cursor()
-    id=123
-    cur.execute("UPDATE times SET last_time = %s WHERE user_id = %s",(times,id))
-    cur.execute("UPDATE times SET last_win = %s WHERE user_id = %s",(winner,id))
-    con.commit()
+    cur.execute("UPDATE times SET last_time = %s WHERE user_id = 123",(times))
+    cur.execute("UPDATE times SET last_win = %s WHERE user_id = 123",(winner))
     con.commit()
     con.close()
 def test_insert():
@@ -22,3 +20,4 @@ def test_insert():
     cur.execute("""INSERT INTO times (user_id,last_time,last_win) VALUES (%s,%s,%s) ON CONFLICT DO NOTHING""", (user,last,win))
     con.commit()
     con.close()
+test_insert()
