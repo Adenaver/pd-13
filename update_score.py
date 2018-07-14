17a7c57639f141781d934a97d1aa94fa2068621a
zhenyok <zhenyok@mail.ua>
2018-07-14 15:01:35 +0300

    fixed path

--- update_score.py
+++ update_score.py
@@ -1,13 +1,9 @@
 import psycopg2
 import os
-#hostname = os.environ.get('hosting')
-#username = os.environ.get('user')
-#password = os.environ.get('password')
-#database = os.environ.get('bdname')
-hostname = "pellefant.db.elephantsql.com"
-username = "axwihbpd"
-password = "FAKqDfFgwFzn8-2Icl3IkXjp77eVuWSR"
-database = "axwihbpd"
+hostname = os.environ.get('hosting')
+username = os.environ.get('user')
+password = os.environ.get('password')
+database = os.environ.get('bdname')
 def new_score(id):
     id=str(id)
     con = psycopg2.connect( host=hostname, user=username, password=password, dbname=database )
@@ -20,4 +16,3 @@ def new_score(id):
     cur.execute("UPDATE users SET id = %s WHERE user_id = %s",(score,id))
     con.commit()
     con.close()
-new_score(376995776)
